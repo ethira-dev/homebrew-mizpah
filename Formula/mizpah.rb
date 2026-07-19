43c5bca74fa19092b06e48415dc11fe454252dac
Lucas de Araújo <lucas@ethira.dev>
2026-07-20 01:51:46 +0200

    mizpah 0.17.9

--- Formula/mizpah.rb
+++ Formula/mizpah.rb
@@ -1,17 +1,17 @@
 class Mizpah < Formula
   desc "JSON log viewer with web UI and MCP for AI agents"
   homepage "https://github.com/ethira-dev/mizpah"
-  version "0.17.8"
+  version "0.17.9"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/ethira-dev/mizpah/releases/download/v0.17.8/mizpah-aarch64-apple-darwin.tar.gz"
-      sha256 "c5f072d23636e30abb2b1a6b244b20f95552bbb25b0d48907f842d270af3e9c7"
+      url "https://github.com/ethira-dev/mizpah/releases/download/v0.17.9/mizpah-aarch64-apple-darwin.tar.gz"
+      sha256 "760932c1f37b908a31435175da0c6ed0ada53cc676c268145bf27ee5395bcc30"
     end
     on_intel do
-      url "https://github.com/ethira-dev/mizpah/releases/download/v0.17.8/mizpah-x86_64-apple-darwin.tar.gz"
-      sha256 "fc3cef2ee12a5e63e3b7abb5743d27ddccbf1640dbf2767231fbef429f1e14d5"
+      url "https://github.com/ethira-dev/mizpah/releases/download/v0.17.9/mizpah-x86_64-apple-darwin.tar.gz"
+      sha256 "1f5a61156d3e0c05756f314181de31e3316ab72d3b10703723835497ff0dba6b"
     end
   end
 
